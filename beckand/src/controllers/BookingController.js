a840c9863b91c5e0a7e1770cfcb91db7d286a897
moisesnfernandes <moiseskaralegal@hotmail.com>
2019-10-01 20:29:42 -0300

    uploud
    beckand quase finalizado

---
+++ beckand/src/controllers/BookingController.js
@@ -0,0 +1,7 @@
+module.export = {
+async store( raq , res ) {
+    const { user_id } = req.headers;
+    const { spot_id } = req.params;
+    const { date } = req.body;
+}
+};
--- beckand/src/routes.js
+++ beckand/src/routes.js
@@ -5,13 +5,19 @@ const UploadComfig = require('./config/Upload')
 const SessionController = require('./controllers/SessionController')
 const spotController = require('./controllers/Spotcontroller')
 const Deshbordcontroller = require('./controllers/Deshbordcontroller')
+const BookingController = require('./controllers/BookingController')
 
 const Upload = multer(UploadComfig)
 const routes = express.Router();
 
+routes.post('/spots/:spot_id/Booking',BookingController.store);
+
 routes.get('/Deshbord', Deshbordcontroller.Show);
-routes.get('/sports', spotController.index);
+
+routes.get('/spots', spotController.index);
+
 routes.post('/Sessions', SessionController.store);
-routes.post('/sports', Upload.single('tumbnail') , spotController.Storage);
+
+routes.post('/spots', Upload.single('tumbnail') , spotController.Storage);
 
 module.exports = routes;
