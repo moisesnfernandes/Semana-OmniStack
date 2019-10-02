8adc456a1ee4b007dc84464091191423db2f5aa1
moisesnfernandes <moiseskaralegal@hotmail.com>
2019-10-02 09:40:26 -0300

    atualizando

--- beckand/src/config/Upload.js
+++ beckand/src/config/Upload.js
@@ -1,15 +1,15 @@
 const multer = require('multer');
 const path = require('path');
 
+
 module.exports = {
- storage: multer.diskStorage({
- destination: path.resolve(__dirname,'..','..','Uploads'),
- filename: (raq , file , Cb) => {
-    const ext = path.extname(file.originalname);
-    const name = path.basename(file.originalname, ext);
+  storage: multer.diskStorage({
+    destination: path.resolve(__dirname, '..', '..', 'uploads'),
+    filename: (req, file, cb) => {
+      const ext = path.extname(file.originalname);
+      const name = path.basename(file.originalname, ext);
 
-     Cb(null, `${name}-${Date.now()}-${ext}`)
-
- }
- })
-};
+      cb(null, `${name}-${Date.now()}${ext}`);
+    }
+  })
+}
--- beckand/src/models/Spot.js
+++ beckand/src/models/Spot.js
@@ -1,15 +1,14 @@
 const mongoose = require('mongoose');
 
 const SpotSchema = new mongoose.Schema({
-    thumbnail : String,
-    company : String,
-    price : Number,
-    techs : [String],
-    user:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref : 'User'
-    }
+  thumbnail: String,
+  company: String,
+  price: Number,
+  techs: [String],
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  }
+})
 
-});
-
-module.exports = mongoose.model('spot', SpotSchema);
+module.exports = mongoose.model('Spot', SpotSchema);
