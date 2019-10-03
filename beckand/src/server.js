057105b056fdb8fff47b40f6efe788342cb81832
moisesnfernandes <moiseskaralegal@hotmail.com>
2019-10-03 11:45:51 -0300

    criando interface web

--- beckand/src/server.js
+++ beckand/src/server.js
@@ -6,14 +6,11 @@ require('dotenv').config()
 const app = express()
 
 
-mongoose.connect(process.env.DB_URL, {
+mongoose.connect('mongodb://zeus:zeus9651@app01-shard-00-00-nswkz.mongodb.net:27017,app01-shard-00-01-nswkz.mongodb.net:27017,app01-shard-00-02-nswkz.mongodb.net:27017/test?ssl=true&replicaSet=app01-shard-0&authSource=admin&retryWrites=true&w=majority',{
     useNewUrlParser: true,
-    useUnifiedTopology: true
-}).catch(error => {
-    console.error ('\x1b[31m',"MONGOB não conectou! Você colocou a URL de conexão no arquivo arquivo .env ?")
-});
+    useUnifiedTopology: true,
 
-
+})
 app.use(express.json())
 app.use(routes)
 
